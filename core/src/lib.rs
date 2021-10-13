d01d92151d8abcfed5a6b28fb12c3cc376014c62
Stephen Akridge <sakridge@gmail.com>
2021-10-13 12:51:53 +0000

    Move test-validator to own module to reduce core dependencies

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -49,7 +49,6 @@ pub mod sigverify;
 pub mod sigverify_shreds;
 pub mod sigverify_stage;
 pub mod snapshot_packager_service;
-pub mod test_validator;
 pub mod tower_storage;
 pub mod tpu;
 pub mod tree_diff;
--- test-validator/src/lib.rs
+++ test-validator/src/lib.rs
@@ -1,9 +1,10 @@
 use {
-    crate::{
+    log::*,
+    solana_client::rpc_client::RpcClient,
+    solana_core::{
         tower_storage::TowerStorage,
         validator::{Validator, ValidatorConfig, ValidatorStartProgress},
     },
-    solana_client::rpc_client::RpcClient,
     solana_gossip::{
         cluster_info::{ClusterInfo, Node},
         gossip_service::discover_cluster,
@@ -193,7 +194,7 @@ impl TestValidatorGenesis {
             info!("Fetching {} over RPC...", address);
             let account = rpc_client.get_account(&address).unwrap_or_else(|err| {
                 error!("Failed to fetch {}: {}", address, err);
-                crate::validator::abort();
+                solana_core::validator::abort();
             });
             self.add_account(address, AccountSharedData::from(account));
         }
